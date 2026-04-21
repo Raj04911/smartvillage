b7eeec48ae2251dbea3bc32ae0c3d9b1845c371f
Ayush Kumar Mishra <ayushkumarmishra@Ayushs-MacBook-Air.local>
2026-04-22 00:11:06 +0530

    Edits done

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     await mongoose.connect("mongodb+srv://kumarsharmaraj256_db_user:TdW9WdN1aqPNvATA@cluster0.qmulcz7.mongodb.net/");
     console.log("MongoDB Connected Successfully");
   } catch (error) {
-    console.error("MongoDB Connection Failed:", error.message);
+    console.error("MongoDB Connection Failed!", error.message);
   }
 };
 
