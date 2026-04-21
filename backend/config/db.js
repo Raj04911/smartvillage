521bb7a3c8b29f1b24f46a6e4a69ff955419403a
Ayush Kumar Mishra <ayushkumarmishra@Ayushs-MacBook-Air.local>
2026-04-22 00:03:08 +0530

    Edits done

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/smartvillage");
+    await mongoose.connect("mongodb+srv://kumarsharmaraj256_db_user:TdW9WdN1aqPNvATA@cluster0.qmulcz7.mongodb.net/");
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed:", error.message);
